1b37e5223b5e2d6fd8e80ed368e2cb2ba1921080
JeysonAmado <89466558+JeysonAmado@users.noreply.github.com>
2022-08-16 11:18:42 -0500

    Fullstack - Mostrar texto encriptado y copiarlo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Encriptador-de-texto---Alura-Challenges-ONE
+
+Encriptador y Desencriptador para Alura Challenges ONE desarrollado por medio de HTML, CSS y Javascript. En la cual se va a tener en cuenta los siguientes criterios de encriptación:
+ 
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
--- encrypt.js
+++ encrypt.js
@@ -6,26 +6,89 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const encrypt = document.querySelector('.encrypt')
 const decrypt = document.querySelector('.decrypt')
+const copy=document.querySelector('.copy')
 const entrada = document.querySelector("textarea")
+
+
+function createDivResult(entradaTexto){
+    const result=document.createElement('div')
+    result.classList.add('result')
+    
+
+    const texto=document.createElement('h2')
+    texto.textContent=entradaTexto
+    
+
+    const boton=document.createElement('input')
+    boton.classList.add('copy')
+    boton.type='submit'
+    boton.value="Copiar"
+    
+
+    result.appendChild(texto)
+    result.appendChild(boton)
+    
+
+    const cuerpo=document.querySelector('body')
+    cuerpo.appendChild(result)
+
+}
+
+function agregarTexto(textoIngresado){
+    let texto=document.querySelector('.result h2')
+    texto.textContent=textoIngresado
+
+    const result=document.querySelector('.result')
+    result.style.display='block'
+    console.log(result)
+    console.log(texto)
+
+    const empty=document.querySelector('.empty')
+    empty.style.display='none'
+}
+
+function noText(){
+    
+    const result=document.querySelector('.result')
+    result.style.display='none'
+   
+
+    const empty=document.querySelector('.empty')
+    empty.style.display='block'
+
+}
+
+function copiar(){
+    var codigoACopiar = document.querySelector('.result h2');
+    var seleccion = document.createRange();
+    seleccion.selectNodeContents(codigoACopiar);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(seleccion);
+    var res = document.execCommand('copy');
+    window.getSelection().removeRange(seleccion);
+
+    
+}
 
 function encriptar(){
     let encriptado=""
     if(entrada.value!=""){
         let input = entrada.value
-        console.log(cadenaAux)
+        
 
         input=input.replaceAll("e",'enter')
         input=input.replaceAll('i','imes')
         input=input.replaceAll('a','ai')
         input=input.replaceAll('o','ober')
         input=input.replaceAll('u','ufat')
-       
+        
+        agregarTexto(input.trimEnd())
         console.log(input.trimEnd())
       
     }
 
     else{
-        alert("espacio vacio")
+        noText()
     }
     
 }
@@ -41,22 +104,22 @@ function desencriptar(){
         input=input.replaceAll('ober','o')
         input=input.replaceAll('ufat','u')
 
-
-       
-        
-       
+        agregarTexto(input.trimEnd())
         console.log(input.trimEnd())
       
     }
 
     else{
-        alert("espacio vacio")
+        noText()
     }
     
 }
 
+
+
 encrypt.onclick=encriptar
 decrypt.onclick=desencriptar
+copy.onclick=copiar
 /* encrypt.addEventListener('click', () =>{
     console.log('Hizo Click')
 }) */
